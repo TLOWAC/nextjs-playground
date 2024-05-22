ef2c80f21aa0ed87591c6ebee565231f4e38aff5
TLOWAC <leechanghun55@gmail.com>
2024-05-22 10:09:53 +0900

    setting: db 셋팅

--- nextjs-t3-app/src/app/page.tsx
+++ nextjs-t3-app/src/app/page.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import { db } from "~/server/db";
 
 const mockUrl = [
   "https://images.unsplash.com/photo-1425082661705-1834bfd09dca?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aGFtc3RlcnxlbnwwfHwwfHx8MA%3D%3D",
@@ -12,10 +13,15 @@ const mockImages = mockUrl.map((url, index) => ({
   url,
 }));
 
-export default function HomePage() {
+export default async function HomePage() {
+  const posts = await db.query.posts.findMany();
+
   return (
     <main className="flex min-h-screen flex-col text-white">
       <div className="container flex flex-wrap gap-12">
+        {posts.map((post) => (
+          <div key={post.id}>{post.name}</div>
+        ))}
         {mockImages.map((image) => (
           <div
             key={image.id}
