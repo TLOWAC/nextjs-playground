091e20934b68f0081ad1546b171d61ce9ca5a93f
TLOWAC <leechanghun55@gmail.com>
2024-05-23 20:21:51 +0900

    refactor: home 페이지 권한 인증 기능 추가

--- nextjs-t3-app/src/app/page.tsx
+++ nextjs-t3-app/src/app/page.tsx
@@ -1,25 +1,33 @@
 import { db } from "~/server/db";
+import { images } from "~/server/db/schema";
+import { SignedOut, SignedIn } from "@clerk/nextjs";
 
 export const dynamic = "force-dynamic";
 
-export default async function HomePage() {
+export async function Images() {
   const images = await db.query.images.findMany({
     orderBy: (model, { asc }) => asc(model.id),
   });
 
   return (
+    <div className="container flex flex-wrap gap-12">
+      {images.map((image) => (
+        <div key={image.id} className="w-48 overflow-hidden rounded shadow-lg">
+          <img src={image.url} alt={`Image ${image.id}`} />
+          <p>{image.name}</p>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default async function HomePage() {
+  return (
     <main className="flex min-h-screen flex-col text-white">
-      <div className="container flex flex-wrap gap-12">
-        {images.map((image) => (
-          <div
-            key={image.id}
-            className="w-48 overflow-hidden rounded shadow-lg"
-          >
-            <img src={image.url} alt={`Image ${image.id}`} />
-            <p>{image.name}</p>
-          </div>
-        ))}
-      </div>
+      <SignedOut>로그인이 필요 합니다.</SignedOut>
+      <SignedIn>
+        <Images />
+      </SignedIn>
     </main>
   );
 }
